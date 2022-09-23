f68dbcccbfc3f5806d37e28310e49e12ca660c5a
hademath <hademath1992@gmail.com>
2022-09-23 18:48:50 +0100

    added postgres database configuration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('is_admin');
+            $table->string('status');
             $table->timestamps();
         });
     }
